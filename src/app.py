00d5222c965ab1b7712bb5a5adc3f8da0de105b0
felipeCaetano <felipecmelo@gmail.com>
2025-12-31 18:42:46 -0300

    usando .json

--- src/app.py
+++ src/app.py
@@ -1,62 +1,84 @@
-import ast
+import json
 import sys
 
 running = True
+FILE_NAME = 'bet.json'
 
 
 def add_new_bet():
-    with open('bet.dat', 'a') as f:
-        print('Digite sua aposta separando os números com espaços\nDigite '
-              'enter para encerrar\n\n>>Ex. 1 2 3 4 5 6<ENTER>')
-        bet = input('>> ')
-        try:
-            aposta = [int(x) for x in bet.split()]
-            f.writelines(f'{str(aposta)}\n')
-        except ValueError:
-            print("Aposta deve ser com numeros")
-            return
-        print("Bet adicionado com sucesso")
+    print(
+        'Digite sua aposta separando os números com espaços\n'
+        'Digite enter para encerrar\n\n>> Ex. 1 2 3 4 5 6'
+    )
+
+    bet_input = input('>> ').strip()
+
+    try:
+        aposta = [int(x) for x in bet_input.split()]
+    except ValueError:
+        print("Aposta deve conter apenas números")
+        return
+
+    registro = {"bet": aposta}
+
+    with open(FILE_NAME, 'a', encoding='utf-8') as f:
+        f.write(json.dumps(registro) + '\n')
+
+    print("Bet adicionado com sucesso")
 
 
 def verify_results():
-    with open('bet.dat', 'r') as f:
-        try:
-            resultado = [
-                int(x) for x in input('insira as dezenas sorteadas '
-                                             'separadas por espaço e pressione '
-                                               'Enter\n\n>>'). strip().split()
-            ]
+    try:
+        resultado = [
+            int(x) for x in input(
+                'Insira as dezenas sorteadas separadas por espaço\n\n>> '
+            ).split()
+        ]
+    except ValueError:
+        print("Os resultados devem ser com números")
+        return
 
-            for index, bet in enumerate(f.readlines(), 1):
-                print(index, bet)
-                bet = ast.literal_eval(bet.strip())
-                hits = set(bet).intersection(set(resultado))
-                num_hits = len(hits)
-                print(resultado, bet, num_hits)
-                if num_hits < 4:
-                    print('Que pena! Não foi dessa vez... :(')
-                elif num_hits == 4:
-                    print(f'Você tirou uma quadra com {bet}!!\nParabéns')
-                elif num_hits == 5:
-                    print(f'Você tirou uma quina com {bet}!!\nParabéns')
-                elif num_hits == 6:
-                    print(f'Você tirou a sorte grande {bet}!!\nParabéns')
-        except ValueError:
-            print("Os resultados devem ser com números")
+    with open(FILE_NAME, 'r', encoding='utf-8') as f:
+        for index, line in enumerate(f, 1):
+            try:
+                registro = json.loads(line)
+                bet = registro["bet"]
+            except (json.JSONDecodeError, KeyError):
+                print(f"Aposta inválida na linha {index}, ignorando")
+                continue
 
+            hits = set(bet).intersection(resultado)
+            num_hits = len(hits)
+
+            print(f"Aposta {index}: {bet} → {num_hits} acertos")
+
+            if num_hits < 4:
+                print('Que pena! Não foi dessa vez... :(')
+            elif num_hits == 4:
+                print(f'Você tirou uma quadra com {bet}!!\nParabéns\n')
+            elif num_hits == 5:
+                print(f'Você tirou uma quina com {bet}!!\nParabéns\n')
+            elif num_hits == 6:
+                print(f'Você tirou a sorte grande {bet}!!\nParabéns\n')
 
 
 while running:
-    option = input("Digite sua opção:\n 1 - cadastrar nova aposta\n 2 - "
-                   "Verificar resultado\n 0 - Sair\n\n>>")
+    option = input(
+        "Digite sua opção:\n"
+        "1 - cadastrar nova aposta\n"
+        "2 - verificar resultado\n"
+        "0 - sair\n\n>> "
+    )
+
     try:
         option = int(option)
-        if option == 1:
-            add_new_bet()
-        if option == 2:
-            verify_results()
-        if option == 0:
-            running = False
-            sys.exit(0)
     except ValueError:
         print("Digite uma opção válida")
+        continue
+
+    if option == 1:
+        add_new_bet()
+    elif option == 2:
+        verify_results()
+    elif option == 0:
+        sys.exit(0)
